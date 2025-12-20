74b56dcfdf1a2fad7e67b0f7528d8ddac3a0aecc
Mykyta Statiev <pcgalaxy123@gmail.com>
2025-12-20 11:22:14 +0200

    Add snowfield animation to enhance festive theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="snowfield" aria-hidden="true" />
         {children}
       </body>
     </html>
